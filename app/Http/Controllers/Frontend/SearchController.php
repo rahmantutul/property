8a0e879ea39fa1a80da23aecda705d4237280e29
Syed Shazeedul Islam <syedshazeedul@gmail.com>
2023-04-05 12:05:14 +0600

    search property complete

--- app/Http/Controllers/Frontend/SearchController.php
+++ app/Http/Controllers/Frontend/SearchController.php
@@ -12,52 +12,53 @@ use App\Models\PropertyDetails;
 class SearchController extends Controller
 {
     /**
-     * 
+     *
      */
     public function searchProperty(Request $request)
     {
-        $property_category = PropertyCategory::with('property')->where('categoryId', $request->category)->get();
-        $propertyCategoryData = $property_category->map(function($propertyCategory) {
-            return $propertyCategory->property;
-        });
-        $property_details = PropertyDetails::with('property')->where(function($q){
-            if (request()->filled('beds')) {
-                return $q->where('numOfBedroom', request()->beds);
-            }
-            if (request()->filled('baths')) {
-                return $q->where('numOfBathroom', request()->baths);
-            }
-        })->get();
-        $propertyDetailsData = $property_details->map(function($propertyDetails) {
-            return $propertyDetails->property;
-        });
-        $property_address = PropertyAddress::with('property')->where(function($q){
-            if (request()->filled('keyword')) {
-                $q->where('streetAddressOne', 'like', '%' . request()->keyword . '%')
-                     ->orWhere('streetAddressTwo', 'like', '%' . request()->keyword . '%');
-            }
-        })->get();
-        $propertyAddressData = $property_address->map(function($propertyAddress) {
-            return $propertyAddress->property;
-        });
-        // dd($propertyAddressData);
-        $query = Property::whereNull('deleted_at')->where('status', 1)
-            ->where(function($q){
-                    if (request()->filled('min_price')) {
-                        $q->where('price', '>=', request()->min_price);
-                    }
-                    if (request()->filled('max_price')) {
-                        $q->where('price', '<=', request()->max_price);
-                    }
-                })->get();
+        $request->validate([
+            'category' => 'nullable|integer',
+            'beds' => 'nullable|integer',
+            'baths' => 'nullable|integer',
+            'min_price' => 'nullable|integer',
+            'max_price' => 'nullable|integer',
+            'keyword' => 'nullable|string',
+        ]);
 
-        $query = $query->merge($propertyCategoryData, $propertyDetailsData, $propertyAddressData);
+        $query = Property::with('propertyCategory')
+            ->whereHas('propertyCategory', function ($q) {
+                if (request()->filled('category')) {
+                    $q->where('categoryId', request()->category);
+                }
+            })
+            ->whereHas('details', function ($q) {
+                if (request()->filled('beds')) {
+                    $q->where('numOfBedroom', request()->beds);
+                }
+                if (request()->filled('baths')) {
+                    $q->where('numOfBathroom', request()->baths);
+                }
+            })
+            ->whereHas('address', function ($q) {
+                if (request()->filled('keyword')) {
+                    $q->where('streetAddressOne', 'like', '%' . request()->keyword . '%')
+                        ->orWhere('streetAddressTwo', 'like', '%' . request()->keyword . '%');
+                }
+            })
+            ->where(function ($q) {
+                if (request()->filled('min_price')) {
+                    $q->where('price', '>=', request()->min_price);
+                }
+                if (request()->filled('max_price')) {
+                    $q->where('price', '<=', request()->max_price);
+                }
+            })
+            ->whereNull('deleted_at')
+            ->where('status', 1)
+            ->paginate(10);
 
-        dd($query);
-
-        
-        
-        $properties = $query->paginate(10);
+            dd($query);
+            $properties = $query->paginate(10);
         return view('frontend.search', compact('properties'));
 
     }
